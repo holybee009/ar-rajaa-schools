2661d9c37846b7cb6d15196e5f9af8a04ec3fae7
holybee009 <135528189+holybee009@users.noreply.github.com>
2024-09-26 16:59:11 +0100

    Update index.tsx

--- app/components/atoms/staffData/index.tsx
+++ app/components/atoms/staffData/index.tsx
@@ -1,32 +1,49 @@
-"use client"
-import Image from "next/image";
+'use client'
+import Button from "../button"
+import StaffBlock from "../staffBlock"
 
+interface Staff{
+    _id: string,
+    selectedYear: string,
+    staffClass: string,
+    staffName: string,
+    uploadedFileUrl: string
+}
 
 interface Props {
-    src: string;
-    title: string;
+    data: Staff[]
+    error: string | null,
+    loading: boolean,
+    handleBack: () => void
 }
 
-const StudentBlock = ({src, title} : Props)  =>{
+const StaffData = ({data,loading,error, handleBack}: Props) =>{
 
-
-
-    return (
-        <>
-        <div className="relative flex flex-col items-center justify-center p-0">  
-          <div className="relative w-full h-24"> 
-            <Image
-                src={src}
-                alt="Description"
-                layout="fill"
-                objectFit="cover"  // Use Tailwind's utility class for object-fit
-                className="absolute inset-0 rounded-xl"
-            />
-          </div>
-            <p className="text-center text-sm leading-3 justify-self-end mt-2 capitalize">{title}</p>
+    return <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : 
+      data.length > 0 ? 
+      (
+        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
+        {
+        data.slice().reverse().map((datas) => 
+         <StaffBlock 
+            key={datas._id}
+            src={datas.uploadedFileUrl}
+            title={datas.staffName}
+        />
+        )
+        }
         </div>
-        </>
-    )
+      ) : (
+        <p>No post yet</p>
+      )}
+      <Button href="#" text="back" onClick={() => handleBack()} className="absolute right-3 bottom-3 w-full inset-x-0"/>
+    </div>
 }
 
-export default StudentBlock;
+export default StaffData;
+
