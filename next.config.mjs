67e3d68a25a92ce3464322adf56f0a50478e367e
holybee009 <135528189+holybee009@users.noreply.github.com>
2024-09-27 07:53:11 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'ar-rajaa-schools-backend.onrender.com',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+        domains: ['ar-rajaa-schools-backend.onrender.com'],
       },
 };
 
