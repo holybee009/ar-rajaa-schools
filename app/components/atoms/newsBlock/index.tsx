036d60db1266f4250d85dc33e545d1e63fb342f2
holybee009 <135528189+holybee009@users.noreply.github.com>
2024-10-01 10:58:53 +0100

    Update index.tsx

--- app/components/atoms/newsBlock/index.tsx
+++ app/components/atoms/newsBlock/index.tsx
@@ -15,7 +15,7 @@ const NewsBlock: React.FC<NewsBlockProps> = ({ src, title, content }) => {
   return (
     <>
     <div className='flex flex-col h-full bg-[#D9D9D9] w-40 h-56 pb-2'>
-        <Image src={src} alt='photo' width={80} height={80} className='w-40'/>
+        <Image src={src} alt='photo' width={80} height={80} className='w-40 h-36'/>
         <p className="text-left text-md font-bold capitalize truncate pt-2 px-2">{title}</p>
         <div className="w-full max-w-lg h-20 text-sm overflow-hidden text-ellipsis px-2 ">
           <p className="line-clamp-3 leading-1">
